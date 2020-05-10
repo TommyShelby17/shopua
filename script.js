c8b9e885ba5e7825c0512797ca88b029b3a5b1c5
SerGucci <klymchuk.buisness@gmail.com>
2020-05-10 18:58:18 +0300

    Added nice-working slider

--- script.js
+++ script.js
@@ -31,18 +31,38 @@ function burgerMenu(selector){
 
 burgerMenu('.burger-menu');
 
-function slider(){
-	let billboardImage = $('.header_billboard');
-	let billboardItems = $('.billboard_items');
-	console.log(billboardImage);
+$(document).ready(function() {
+  
+  var width = $('#slider-container').width();
+  //dimensions
+  $('.slides>li').width(width);
+  $('.slides').width(width * $('.slides>li').length);
 
-	billboardImage.on('click' , () =>{
-		billboardImage.css({
-			'background':'url("images/billboard_mobile.png") no-repeat center',
-			'background-size':'100% 100%',
-		 	});
-		billboardItems.css('color', '#030803');
-	})
-}
-
-slider();
+  
+  //positioning
+   $('.slides').css('left',-width);
+   $('.slides>li:last-child').prependTo('.slides');
+  
+  //move slides forward
+  function nextSlide() {
+    $('.slides').animate({
+      'margin-left':-width
+    },500, function() {
+      $('.slides>li:first-child').appendTo('.slides');
+      $('.slides').css('margin-left', 0);
+    });
+  }
+  //move slides backwards
+  function prevSlide() {
+    $('.slides').animate({
+      'margin-left':width
+    },500, function() {
+      $('.slides>li:last-child').prependTo('.slides');
+      $('.slides').css('margin-left', 0);
+    });
+  }
+    
+  //controls
+  $('.next').click(nextSlide);
+  $('.prev').click(prevSlide)
+});
